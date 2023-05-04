1cfa8537d4e225ef27ad06063b377e602bbfc0f0
D-tech2023 <129057069+D-tech2023@users.noreply.github.com>
2023-05-04 13:19:03 +0300

    Add files via upload

---
+++ employer_detail.java
@@ -0,0 +1,36 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "employers")
+
+public class employer_detail {
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+}
+
+
